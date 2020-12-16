a7ae971cf837ab16af5a392b09ff28d84f8f350b
Emmanuel Daniels <emmanueldaniels37@gmail.com>
2020-12-15 20:37:11 -0500

    added signup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Projects from '../views/Projects.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Projects from '../views/Projects.vue';
+import Signup from "../views/Signup.vue";
+
 
 Vue.use(VueRouter)
 
@@ -23,6 +25,9 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: "/signup", name: "signup", component: Signup
   }
 ]
 
